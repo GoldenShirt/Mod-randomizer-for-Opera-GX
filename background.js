ec80825f441bd958995a6bdfe873231f2b072c2d
GoldenShirt <56863502+GoldenShirt@users.noreply.github.com>
2024-03-01 01:05:52 +0200

    Add files via upload
    V2.4

--- background.js
+++ background.js
@@ -1,69 +1,70 @@
 let randomizeOnStartup;
+
 chrome.runtime.onInstalled.addListener(function () {
-  // Set initial value for modExtensionIds in storage
+  // Set initial values for extension settings in storage
   randomizeOnStartup = true;
-  chrome.storage.local.set({ modExtensionIds: [], randomizeOnStartup: randomizeOnStartup ,toggleRandomizeOnStartupChecked: randomizeOnStartup, autoModIdentificationChecked: true}, function () {
-    console.log('Mod Randomizer: Extension installed. Initializing settings. randomizeOnStartup is set to ', randomizeOnStartup, ' autoModIdentification is set to ' , true);
+
+  const initialSettings = {
+    modExtensionIds: [],
+    randomizeOnStartup: randomizeOnStartup,
+    toggleRandomizeOnStartupChecked: randomizeOnStartup,
+    autoModIdentificationChecked: true,
+  };
+
+  chrome.storage.local.set(initialSettings, function () {
+    console.log('Mod Randomizer: Extension installed. Initializing settings.', initialSettings);
   });
 });
 
-chrome.runtime.onStartup.addListener(function () {
-  // Run your startup logic here
-  console.log('Running startup code');
-  runStartupLogic();
-});
+chrome.runtime.onStartup.addListener(runStartupLogic);
 
 function runStartupLogic() {
   // Use a default value if randomizeOnStartup is not defined
-  randomizeOnStartup = randomizeOnStartup !== undefined ? randomizeOnStartup : true;
+  randomizeOnStartup = randomizeOnStartup ?? true;
 
   console.log('After setting default: randomizeOnStartup is set to ', randomizeOnStartup);
 
-  chrome.storage.local.get({ randomizeOnStartup: randomizeOnStartup }, function (result) {
-    // Use the global variable instead of declaring a new one
+  chrome.storage.local.get({ randomizeOnStartup: randomizeOnStartup, modExtensionIds: [] }, function (result) {
     randomizeOnStartup = result.randomizeOnStartup;
-    console.log('Result startup setting overiding default: set to ', result.randomizeOnStartup);
+    console.log('Result startup setting overriding default: set to ', randomizeOnStartup);
 
-    if (randomizeOnStartup === true) {
-		
-      // Retrieve mod extension IDs from local storage
-      chrome.storage.local.get('modExtensionIds', function (result) {
-        const modExtensionIds = result.modExtensionIds || [];
+    if (randomizeOnStartup) {
+  // Retrieve mod extension IDs from local storage
+  const modExtensionIds = result.modExtensionIds || [];
 
-        // Get all installed extensions
-        chrome.management.getAll(function (extensions) {
-          // Filter for mod extensions based on stored IDs
-          const modExtensions = extensions.filter(extension => modExtensionIds.includes(extension.id));
+  // Get all installed extensions
+  chrome.management.getAll(extensions => {
+    // Filter for mod extensions based on stored IDs
+    const modExtensions = extensions
+      .filter(extension => modExtensionIds.includes(extension.id))
+      .map(({ id, name, enabled }) => ({ id, name, enabled }));
 
-          console.log('Mod Randomizer: Mod Extensions identified:', modExtensions);
+    console.log('Mod Randomizer: Startup detected. Mod Extensions retrieved and identified:', modExtensions);
 
-          if (modExtensions.length > 0) {
-            // Randomly select a mod extension
-            const randomIndex = Math.floor(Math.random() * modExtensions.length);
-            const selectedExtension = modExtensions[randomIndex];
-            console.log('Mod Randomizer: Randomly selected mod extension:', selectedExtension);
+    if (modExtensions.length > 0) {
+      // Randomly select a mod extension
+      const randomIndex = Math.floor(Math.random() * modExtensions.length);
+      const selectedExtension = modExtensions[randomIndex];
+      console.log('Mod Randomizer: Randomly selected mod extension:', selectedExtension);
 
-            // Disable all mod extensions sequentially
-            modExtensions.forEach(function (extension) {
-              if (extension.id !== selectedExtension.id) {
-                chrome.management.setEnabled(extension.id, false, function () {
-                  console.log('Mod Randomizer: Disabled mod extension:', extension.name);
-                });
-              }
-            });
-
-            // Enable the randomly selected mod extension
-            if (!selectedExtension.enabled) {
-              chrome.management.setEnabled(selectedExtension.id, true, function () {
-                console.log('Mod Randomizer: Enabled selected mod extension:', selectedExtension.name);
-              });
-            }
-          }
-        });
+      // Disable all mod extensions sequentially
+      modExtensions.forEach(extension => {
+        if (extension.id !== selectedExtension.id) {
+          chrome.management.setEnabled(extension.id, false, () => {
+            console.log('Mod Randomizer: Disabled mod extension:', { id: extension.id, name: extension.name, enabled: extension.enabled });
+          });
+        }
       });
+
+      // Enable the randomly selected mod extension
+      if (!selectedExtension.enabled) {
+        chrome.management.setEnabled(selectedExtension.id, true, () => {
+          console.log('Mod Randomizer: Enabled selected mod extension:', { id: selectedExtension.id, name: selectedExtension.name, enabled: selectedExtension.enabled });
+        });
+      }
     }
   });
-  
-  
- 
-};
+}
+
+  });
+}
--- popup.js
+++ popup.js
@@ -1,115 +1,95 @@
-document.addEventListener('DOMContentLoaded', function() {
- 
+document.addEventListener('DOMContentLoaded', function () {
   // Get elements by ID
   const extensionList = document.getElementById('extensionList');
   const randomizeButton = document.getElementById('randomizeButton');
   const toggleAutoModIdentification = document.getElementById('toggleAutoModIdentification');
   const toggleRandomizeOnStartupChecked = document.getElementById('toggleRandomizeOnStartup');
   const modForm = document.getElementById('modForm');
-	
-  // Event listeners for checkbox state changes
-  toggleAutoModIdentification.addEventListener('change', function () {
-    handleAutoModIdentificationChange();
-  });
 
-  toggleRandomizeOnStartupChecked.addEventListener('change', function () {
-    handleRandomizeOnStartupChange();
-  });
-  
-  modForm.addEventListener('submit', function(event) {
-    handleFormSubmission(event);
-  });
-  
-  randomizeButton.addEventListener('click', function() {
-	  handleRandomizeButtonClick();
-  });
+  // Event listeners for checkbox state changes
+  toggleAutoModIdentification.addEventListener('change', handleAutoModIdentificationChange);
+  toggleRandomizeOnStartupChecked.addEventListener('change', handleRandomizeOnStartupChange);
+  modForm.addEventListener('submit', handleFormSubmission);
+  randomizeButton.addEventListener('click', handleRandomizeButtonClick);
 
   // Retrieve saved state of checkboxes on popup open
-  chrome.storage.local.get(['autoModIdentificationChecked', 'toggleRandomizeOnStartupChecked', 'randomizeOnStartup: randomizeOnStartup'], function (result) {
+  chrome.storage.local.get(
+  ['autoModIdentificationChecked', 'toggleRandomizeOnStartupChecked', 'randomizeOnStartup'],
+  result => {
     toggleAutoModIdentification.checked = result.autoModIdentificationChecked || false;
     toggleRandomizeOnStartupChecked.checked = result.toggleRandomizeOnStartupChecked || false;
-	
+
     if (toggleAutoModIdentification.checked) {
       identifyModExtensions();
-    } 
-	else {
-	simplifiedUpdateCheckboxes();
-	}
-	
-  });
+    } else {
+      simplifiedUpdateCheckboxes();
+    }
+  }
+);
+
 
   // Function to update checkmarks without variables
   function simplifiedUpdateCheckboxes() {
-  chrome.management.getAll(function(extensions) {
-    chrome.storage.local.get('modExtensionIds', function(result) {
-      var retrievedModExtensionIds = result.modExtensionIds || [];
-      updateCheckboxes(extensions, retrievedModExtensionIds);
+    chrome.management.getAll(extensions => {
+      chrome.storage.local.get('modExtensionIds', ({ modExtensionIds }) => {
+        updateCheckboxes(extensions, modExtensionIds || []);
+      });
     });
-  });
-}
+  }
 
   // Function to update checkboxes based on extension IDs
   function updateCheckboxes(extensions, modExtensionIds) {
-  var extensionListContainer = document.getElementById('extensionList');
-  // Clear the existing content in case there were checkboxes from a previous state
-  extensionListContainer.innerHTML = '';
-  
-  // Sort extensions alphabetically by name
-  extensions.sort(function (a, b) {
-    return a.name.localeCompare(b.name);
-  });
-  
-  extensions.forEach(function (extension) {
-    if (extension.id !== 'toggleAutoModIdentification' && extension.id !== 'toggleRandomizeOnStartup') {
-      var label = document.createElement('label');
-      var checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.id = extension.id;
-      checkbox.checked = modExtensionIds.includes(extension.id);
+    const extensionListContainer = document.getElementById('extensionList');
+    // Clear the existing content
+    extensionListContainer.innerHTML = '';
 
-      label.appendChild(checkbox);
-      label.appendChild(document.createTextNode(extension.name));
+    // Sort extensions alphabetically by name
+    extensions.sort((a, b) => a.name.localeCompare(b.name));
 
-      extensionListContainer.appendChild(label);
-    }
-  });
-}
-  
+    extensions.forEach(extension => {
+      if (extension.id !== 'toggleAutoModIdentification' && extension.id !== 'toggleRandomizeOnStartup') {
+        const label = document.createElement('label');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = extension.id;
+        checkbox.checked = modExtensionIds.includes(extension.id);
+
+        label.appendChild(checkbox);
+        label.appendChild(document.createTextNode(extension.name));
+
+        extensionListContainer.appendChild(label);
+      }
+    });
+  }
+
   // Function to handle form submission
   function handleFormSubmission(event) {
     event.preventDefault();
 
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    var modExtensionIds = Array.from(checkboxes)
-      .filter(function(checkbox) {
-        // Exclude both "toggleAutoModIdentification" and "toggleRandomizeOnStartup" checkboxes
-        return checkbox.checked && checkbox.id !== 'toggleAutoModIdentification' && checkbox.id !== 'toggleRandomizeOnStartup';
-      })
-      .map(function(checkbox) {
-        return checkbox.id;
-      });
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const modExtensionIds = Array.from(checkboxes)
+      .filter(checkbox => checkbox.checked && checkbox.id !== 'toggleAutoModIdentification' && checkbox.id !== 'toggleRandomizeOnStartup')
+      .map(checkbox => checkbox.id);
 
     // Save the mod extension IDs to local storage
-    chrome.storage.local.set({ modExtensionIds: modExtensionIds }, function() {
+    chrome.storage.local.set({ modExtensionIds }, () => {
       // Send a message to the background script
-      console.log('Save button: modExtensionsSaved', modExtensionIds );
-      window.close();  // Comment out this line when changing the code.
+      console.log('Save button: modExtensionsSaved', modExtensionIds);
+      window.close(); // Comment out this line when changing the code.
     });
   }
-  
+
   // Function to handle "Randomize" button click
   function handleRandomizeButtonClick() {
-  chrome.storage.local.get('modExtensionIds', function(result) {
-    var modExtensionIds = result.modExtensionIds || [];
+    chrome.storage.local.get('modExtensionIds', ({ modExtensionIds }) => {
+      if (modExtensionIds && modExtensionIds.length > 0) {
+        disableAndEnableRandomExtension(modExtensionIds);
+      } else {
+        console.log('Error, mod extensions not found in local storage');
+      }
+    });
+  }
 
-    if (modExtensionIds.length > 0) {
-      disableAndEnableRandomExtension(modExtensionIds);
-    }
-	else {console.log('Error, mod extensions not found in local storage')
-	}
-  });
-}
- 
   // Function to handle "Automatic Mod Identification" checkbox change
   function handleAutoModIdentificationChange() {
     chrome.storage.local.set({ autoModIdentificationChecked: toggleAutoModIdentification.checked });
@@ -123,48 +103,56 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Function to handle "Randomize on Startup" change and save the state
   function handleRandomizeOnStartupChange() {
-    var randomizeOnStartup = toggleRandomizeOnStartupChecked.checked;
-    chrome.storage.local.set({ toggleRandomizeOnStartupChecked: randomizeOnStartup, randomizeOnStartup: randomizeOnStartup });
+    const randomizeOnStartup = toggleRandomizeOnStartupChecked.checked;
+    chrome.storage.local.set({ toggleRandomizeOnStartupChecked: randomizeOnStartup, randomizeOnStartup });
     console.log('Randomize on startup is saved as ', randomizeOnStartup);
   }
-  
+
   // Function to automatically identify mod extensions and save them
   function identifyModExtensions() {
-    chrome.management.getAll(function (extensions) {
-      var modExtensionIds = extensions
-        .filter(function (extension) {
-          return extension.updateUrl === "https://api.gx.me/store/mods/update";
-        })
-        .map(function (extension) {
-          return extension.id;
-        });
+  chrome.management.getAll(extensions => {
+    const modExtensionIds = extensions
+      .filter(extension => extension.updateUrl === 'https://api.gx.me/store/mods/update')
+      .map(extension => extension.id);
 
-      updateCheckboxes(extensions, modExtensionIds);
+    // Filter for mod extensions based on stored IDs
+    const modExtensions = extensions
+      .filter(extension => modExtensionIds.includes(extension.id))
+      .map(({ id, name, enabled }) => ({ id, name, enabled }));
 
-      chrome.storage.local.set({ modExtensionIds: modExtensionIds }, function () {
-        console.log('Automatic Mod Identification: modExtensionsSaved', modExtensionIds );
-        
+    updateCheckboxes(extensions, modExtensionIds);
+
+    chrome.storage.local.set({ modExtensionIds }, () => {
+      console.log('Automatic Mod Identification: Mod Extensions identified:', {
+        modExtensions,
       });
     });
-  }
+  });
+}
+
 
   // Function to disable and enable a random extension
-  function disableAndEnableRandomExtension(modExtensionIds) {
-  modExtensionIds.forEach(function (extensionId) {
-    chrome.management.setEnabled(extensionId, false, function () {
-      console.log('Disabled Extension:', extensionId);
+function disableAndEnableRandomExtension(modExtensionIds) {
+  modExtensionIds.forEach(extensionId => {
+    chrome.management.get(extensionId, extension => {
+      const { id, name, enabled } = extension;
+      chrome.management.setEnabled(extensionId, false, () => {
+        console.log('Disabled Extension:', { id, name, enabled });
+      });
     });
   });
 
-  var randomIndex = Math.floor(Math.random() * modExtensionIds.length);
-  var randomExtensionId = modExtensionIds[randomIndex];
+  const randomIndex = Math.floor(Math.random() * modExtensionIds.length);
+  const randomExtensionId = modExtensionIds[randomIndex];
 
-  setTimeout(function () {
-    chrome.management.setEnabled(randomExtensionId, true, function () {
-      console.log('Enabled Random Extension:', randomExtensionId);
+  setTimeout(() => {
+    chrome.management.get(randomExtensionId, extension => {
+      const { id, name, enabled } = extension;
+      chrome.management.setEnabled(randomExtensionId, true, () => {
+        console.log('Enabled Random Extension:', { id, name, enabled });
+      });
     });
   }, 100);
 }
 
- 
 });
